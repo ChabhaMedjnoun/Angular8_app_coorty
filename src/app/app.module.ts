a93410846511467573cd25b4159ced8945568767
chabha <chabha.medjnoun@gmail.com>
2021-07-02 12:03:02 +0200

    Add file to git

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ListViewModule } from '@syncfusion/ej2-angular-lists';
+import {
+  IgxAvatarModule,
+  IgxFilterModule,
+  IgxIconModule,
+  IgxListModule,
+  IgxInputGroupModule,
+  IgxButtonGroupModule
+} from 'igniteui-angular';
+import { ListSample4Component } from './list-sample-4/list-sample-4.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent, ListSample4Component],
+  imports: [
+    ListViewModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    IgxAvatarModule,
+    IgxFilterModule,
+    IgxIconModule,
+    IgxListModule,
+    IgxInputGroupModule,
+    IgxButtonGroupModule
+  ],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
---
+++ src/app/list-sample-4/list-sample-4.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { IgxFilterOptions } from 'igniteui-angular';
+@Component({
+  selector: 'app-contact-list2',
+  styleUrls: ['./list-sample-4.component.scss'],
+  templateUrl: './list-sample-4.component.html'
+})
+export class ListSample4Component implements OnInit {
+  public searchContact: string;
+
+  public contacts = [
+    {
+      isFavorite: false,
+      name: 'Terrance Orta',
+      email: 'a.medjnoun@gmail.com',
+      photo:
+        'https://www.infragistics.com/angular-demos/assets/images/men/27.jpg'
+    },
+    {
+      isFavorite: true,
+      name: 'Richard Mahoney',
+      email: 'Richar.mahoney@gmail.com',
+      photo:
+        'https://www.infragistics.com/angular-demos/assets/images/men/1.jpg'
+    },
+    {
+      isFavorite: false,
+      name: 'Donna Price',
+      email: 'amine@gmail.com',
+      photo:
+        'https://www.infragistics.com/angular-demos/assets/images/women/50.jpg'
+    },
+    {
+      isFavorite: false,
+      name: 'chabha medjnoun',
+      email: 'chabha.medjnoun@gmail.com',
+      photo:
+        'https://www.infragistics.com/angular-demos/assets/images/women/3.jpg'
+    },
+    {
+      isFavorite: true,
+      name: 'slim. Spencer',
+      email: 'slim.medj@gmail.com',
+      photo:
+        'https://www.infragistics.com/angular-demos/assets/images/women/67.jpg'
+    }
+  ];
+
+  public density = 'Person';
+  public displayDensities;
+
+  constructor() {}
+
+  public ngOnInit() {
+    this.displayDensities = [
+      {
+        label: 'Person List',
+        selected: this.density === 'comfortable',
+        togglable: true
+      },
+      {
+        label: 'compact',
+        selected: this.density === 'compact',
+        togglable: true
+      }
+    ];
+  }
+
+  public selectDensity(event) {
+    this.density = this.displayDensities[event.index].label;
+  }
+
+  public toggleFavorite(contact: any) {
+    contact.isFavorite = !contact.isFavorite;
+  }
+
+  get filterContacts() {
+    const fo = new IgxFilterOptions();
+    fo.key = 'name';
+    fo.inputValue = this.searchContact;
+    return fo;
+  }
+  removeItem(index) {
+    this.contacts.splice(index, 1);
+  }
+}
